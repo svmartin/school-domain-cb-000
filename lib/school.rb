de0397ddc8e83da6e2fb68ccb2a2587d89fe3c4b
Sean Martin <sean.martin@gmail.com>
2018-09-30 20:49:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,10 +7,9 @@ class School
 
   def add_student(student_name, grade)
     if @roster.key?(grade)
-      @roster.grade << student_name
+      @roster[grade] << student_name
     else
-      @roster.grade = []
-      @roster.grade << student_name
+      @roster[grade] = [student_name]
     end
   end
 
